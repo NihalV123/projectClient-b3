3532db7083d122b323bb7d155ebbe7f41298e776
Netra001 <nhlvcam@gmail.com>
2018-03-09 18:53:32 +0530

    friday

--- app/src/main/java/a123/vaidya/nihal/foodcrunchclient/Tutorial.java
+++ app/src/main/java/a123/vaidya/nihal/foodcrunchclient/Tutorial.java
@@ -50,54 +50,55 @@ public class Tutorial extends TutorialActivity {
                 .setDrawable(R.drawable.ic_favorite_black_24dp) // int top drawable
                 .setSummary("ONCE YOU ADD TO FAVORITES ICON WILL CHANGE TO WHITE AND ITEMS WILL BE STORED IN FAVORITE LIST FOR QUICK BUY")
                 .build());
-        addFragment(new Step.Builder().setTitle("SHARE TO FACEBOOK BUTTON")
-                .setContent("OPENS DAILOG TO SHARE FOOD TO FACEBOOK")
+        addFragment(new Step.Builder().setTitle("CHAT BOT")
+                .setContent("ALTHOUGH IN DEVELOPMENT YOU CAN TALK TO THE CHAT BOT")
                       .setBackgroundColor(R.color.colorPrimary)  // int background color
-                .setDrawable(R.drawable.ic_share_black_24dp) // int top drawable
-                .setSummary("SHARE DAILOG IS COMPLETELY ISOLATED WE DO NOT STORE YOUR INFO")
+                .setDrawable(R.drawable.ic_android_black_24dp) // int top drawable
+                .setSummary("TALK TO THE CHAT BOT FOR FUNNY RESPONSES")
                 .build());
-        addFragment(new Step.Builder().setTitle("FOOD DETAILS SCREEN")
-                .setContent("LETS YOU CHHOSE ITEMS GET DETAILS AND RECEPIES ABOUT IT ALSO RATE AND VIEW WHAT OTHER PEOPLE SAID")
+        addFragment(new Step.Builder().setTitle("CHANGE NAME/PASSWORD/EMAIL/HOME ADDRESS OPTIONS")
+                .setContent("LETS YOU CHANGE NAME/PASSWORD/EMAIL/HOME ADDRESS LOG OFF FOR EFFECT")
                        .setBackgroundColor(R.color.about_instagram_color)  // int background color
-                .setDrawable(R.drawable.ic_add_shopping_cart_black_24dp) // int top drawable
-                .setSummary("CART BUTTON ADDS SELECTED QUANTITY TO CART QUANTITY CAN BE CHANGED IN CART TOO ")
+                .setDrawable(R.drawable.ic_view_list_black_24dp) // int top drawable
+                .setSummary("YOU CAN SEE YOUR NAME AND EMAIL CHANGED IN NAVIGATION DRAWER\n YOU CAN CHOOSE HOME ADDRESS" +
+                        " FOR DELIVERING YOUR PRODUCT \nTOCHANGE PASSWORD YOU NEED YOUR UNIQUE SECURE CODE")
                 .build());
         addFragment(new Step.Builder().setTitle("RATING BUTTON")
                 .setContent("YOU CAN GIVE RATING AND PROVIDE FEEDBACK FOR THE FOOD WITH THIS BUTTON")
                      .setBackgroundColor(R.color.colorPrimary)  // int background color
-                .setDrawable(R.drawable.ic_security_black_24dp) // int top drawable
+                .setDrawable(R.drawable.ic_star_black_24dp) // int top drawable
                 .setSummary("YOU CAN PROVIDE FEEDBACK AS MANY TIMES AS YOU WANT AND PREVIOUS FEEDBACK CAN BE VIEWED IN SHOW REVIEWS BUTTON")
                 .build());
-        addFragment(new Step.Builder().setTitle("CART SCREEN")
+        addFragment(new Step.Builder().setTitle("MY CART OPTION")
                 .setContent("YOU CAN CHANGE QUANTITY DELETE ONE OR ALL ITEMS IN CART")
                      .setBackgroundColor(R.color.about_instagram_color)  // int background color
-                .setDrawable(R.drawable.ic_child_care_black_24dp) // int top drawable
-                .setSummary("YOU NEED TO CLCIK PLACE OREDERENTER NEW NAME EXCEEDING MINIMUM LENGTH AND CLICK UPDATE FOR SUCCESSFULLY UPDATING NAME")
+                .setDrawable(R.drawable.ic_shopping_basket_black_24dp) // int top drawable
+                .setSummary("YOU NEED TO CLCIK PLACE OREDER TYPE OR CHOOSE ADDRESS AND PAYMENT METHOD AND SELECT WAY TO BE INFORMED FOR SUCCESSFUL ORDER")
                 .build());
-        addFragment(new Step.Builder().setTitle("TO DO LIST OPTION")
-                .setContent("YOU CAN TEMPORARILY WRITE DOWN A LIST OF THINGS TO BE DONE USING THIS OPTION")
+        addFragment(new Step.Builder().setTitle("GROCERY LIST OPTION")
+                .setContent("YOU CAN TEMPORARILY WRITE DOWN SHOPPING LIST USING THIS OPTION")
                    .setBackgroundColor(R.color.colorPrimary)  // int background color
                 .setDrawable(R.drawable.ic_bookmark_black_24dp) // int top drawable
                 .setSummary("YOU CAN ADD NEW ITEM USING + BUTTON ON THE TOP LEFT AND CLEAR ALL ITEMS USING " +
                         "CLEAR BUTTON \nTHIS LIST IS TEMPORARY AND IS NOT STORED ANYWHERE AND WILL BE DELETED WHEN YOU EXIT THAT SCREEN")
                 .build());
-        addFragment(new Step.Builder().setTitle("SEND NEWS OPTION")
-                .setContent("YOU CAN SEND NOTIFICATIONS TO ALL SUBSCRIBERS ABOUT SPECIAL FOOD OFFERS USING THIS OPTION")
+        addFragment(new Step.Builder().setTitle("SETTINGS OPTION")
+                .setContent("YOU CAN RECIEVE NOTIFICATIONS ABOUT SPECIAL FOOD OFFERS IF YOU SUBSCRIBE USING THIS OPTION")
                         .setBackgroundColor(R.color.about_instagram_color)  // int background color
-                .setDrawable(R.drawable.ic_notifications_active_black_24dp) // int top drawable
-                .setSummary("YOU NEED TO ADD TITLE AND MESSAGE FOR NOTIFICATION AND CLICK SEND")
+                .setDrawable(R.drawable.ic_settings_applications_black_24dp) // int top drawable
+                .setSummary("WE WILL INFORM YOU ABOUT DISCOUNTS FESIVALRELATED PRODUCTS ETC")
                 .build());
-        addFragment(new Step.Builder().setTitle("RESEND LOGIN OPTION")
-                .setContent("IN CASE YOU WANT TO CHECK YOUR CREDENTIALS YOU CAN SEND THEM TO YOUR EMAIL USING THIS OPTION")
+        addFragment(new Step.Builder().setTitle("REMEMBER ME BUTTON")
+                .setContent("YOU CAN RE LOGIN IF THE APP IS CLOSED USING THIS BUTTON")
                       .setBackgroundColor(R.color.colorPrimary)  // int background color
-                .setDrawable(R.drawable.ic_supervisor_account_black_24dp) // int top drawable
-                .setSummary("YOU NEED TO CHOOSE THE APP YOU NEED TO RECIEVE LOGIN DETAILS DEFAULT IS GMAIL BUT YOU CAN USE WHATSAPP AND OTHERS TOO YOU NEED TO FIND YOUR ACCOUNT AND HIT SEND")
+                .setDrawable(R.drawable.ic_info_black_24dp) // int top drawable
+                .setSummary("YOU NEED TO REMAIN LOGGED IN BEFORE THE APP IS CLOSED AND BUTTON MUST BE TICKED FOR IT TO WORK PROPERLY")
                 .build());
-        addFragment(new Step.Builder().setTitle("SIGN OUT OPTION")
-                .setContent("YOU CAN SIGN OUT FROM THIS OPTION ")
+        addFragment(new Step.Builder().setTitle("FORGOT PASSOWRD")
+                .setContent("YOU CAN RECOVER YOUR PASSWORD WITH YOUR SECURE CODE AND PHONE NUMBER")
                         .setBackgroundColor(R.color.about_instagram_color)  // int background color
-                .setDrawable(R.drawable.ic_exit_to_app_black_24dp) // int top drawable
-                .setSummary("IF YOU ARE DONE PURCHASING FOOD OR NEED TO LOG IN AS DIFFERENT USER YOU CAN USE THIS OPTION YOUR CART STILL REMAINS WHEN YOU SIGN OUT ON SAME PHONE")
+                .setDrawable(R.drawable.ic_security_black_24dp) // int top drawable
+                .setSummary("PASSOWRD IS MAPPED TO YOUR PHONE ONLY")
                 .build());
         addFragment(new Step.Builder().setTitle("ABOUT OPTION")
                 .setContent("YOU CAN VIEW DETAILS ABOUT THE CREATOR OF THIS APP WITH THIS OPTION")
